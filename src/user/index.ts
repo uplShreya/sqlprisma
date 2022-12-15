55cd9f36961db276aa5266d47486fc3a26224c3c
uplShreya <shreya.saha@utplco.com>
2022-12-15 15:13:55 +0530

    find many

--- src/user/index.ts
+++ src/user/index.ts
@@ -7,9 +7,9 @@ const router = express.Router();
 /**
  * @type - POST
  * @route -  /api/user
- * @desc - route for contact Us.
+ * @desc - route for create user.
  * @access - PUBLIC
- * @function - createContact
+ * @function - createUser
  */
 
 router.post("/", async (req: any, res: any) => {
@@ -33,5 +33,32 @@ router.post("/", async (req: any, res: any) => {
             });
         })
 })
-
+/**
+ * @type - GET
+ * @route -  /api/user
+ * @desc - route for get all user.
+ * @access - PUBLIC
+ * @function - createContact
+ */
+router.get("/",async(req:any, res:any) => {
+    userServices.getAllUser(req,res)
+    .then((response:any) => {
+        return res.send({
+            status: response.status,
+            error: response.error,
+            result: response.result,
+            code: response.code,
+            message: response.message,
+        });
+    })
+    .catch((err:any) => {
+        return res.send({
+            status: err.status,
+            error: err.error,
+            result: err.result,
+            code: err.code,
+            message: err.message,
+        });  
+    })
+})
 export default router;
--- src/user/services.ts
+++ src/user/services.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import messages from "../services/messages.json";
 
 
 const prisma = new PrismaClient()
@@ -47,6 +48,42 @@ export default{
             }
         })
 
+    },
+    getAllUser:(req:any,res:any)=>{
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const getuser=await prisma.user.findMany()
+                console.log(getuser,"getUser")
+                if(getuser){
+                    return resolve({
+                        status: 200,
+                        error: false,
+                        result: getuser,
+                        code: "USER_FOUND",
+                        message: messages["USER_FOUND"]
+                    })
+                }else{
+                    return reject({
+                        status: 404,
+                        error: true,
+                        code: "USER_NOT_EXISTS",
+                        message:"USER_NOT_EXISTS"
+                    })
+                }
+
+            }catch(err){
+                return reject({
+                        status: 500,
+                        error: true,
+                        result: err,
+                        code: "INTERNAL_SERVER_ERROR",
+                        message: "INTERNAL_SERVER_ERROR"
+                })
+    
+            }
+
+        })
+   
     }
 }
   
